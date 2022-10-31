dd705f80abb9872191c0afe149075ace04c51424
WillYum <liamshere98@gmail.com>
2022-10-31 22:40:05 +0200

    Add random pitch logic for audio

--- Assets/Scripts/Systems/AudioManager.cs
+++ Assets/Scripts/Systems/AudioManager.cs
@@ -82,6 +82,7 @@ namespace AudioClasses
         [SerializeField] public AudioClip Clip;
         [SerializeField][Range(0f, 1.0f)] public float Volume = 1.0f;
         [SerializeField] public bool Loop = false;
+        [SerializeField] public bool RandPitch = false;
     }
 
 
@@ -95,6 +96,7 @@ namespace AudioClasses
         public string Name { get => _name; }
         public bool isPlaying { get => _source.isPlaying; }
 
+        private bool _randPitch = false;
 
         public Audio(AudioSource source, AudioConfig audioConfig)
         {
@@ -104,6 +106,7 @@ namespace AudioClasses
             _source.volume = audioConfig.Volume;
             _source.clip = audioConfig.Clip;
             _source.loop = audioConfig.Loop;
+            _randPitch = audioConfig.RandPitch;
         }
 
         public void SetVolume(float volume)
@@ -114,6 +117,10 @@ namespace AudioClasses
         public void Play()
         {
             _source.Play();
+            if (_randPitch)
+            {
+                _source.pitch = Random.Range(0.8f, 1.0f);
+            }
         }
 
         public void Stop()
