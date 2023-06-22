8ff0448d10c2b9929a2ad8f39b6075bbf451ac23
WillYum <liamshere98@gmail.com>
2023-06-23 00:03:08 +0300

    Add warning comment for an important animation event function

--- Assets/Scripts/GameLogic/Turret/TurretEvents.cs
+++ Assets/Scripts/GameLogic/Turret/TurretEvents.cs
@@ -5,7 +5,7 @@ public class TurretEvents : MonoBehaviour
 {
     public event Action OnTurretShoot;
 
-    //This function is accessed by the animation event
+    //WARNING: This function is accessed by the animation event
     //DON'T REMOVE THIS FUNCTION
     private void ShootBullet()
     {
