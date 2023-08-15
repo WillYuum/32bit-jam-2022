7344740256a441f454807c87e288a24d4296b610
WillYum <liamshere98@gmail.com>
2023-08-15 14:16:51 +0300

    Implement Spawner restructure
    - Move Spawner out of Systems folder.
    - Move all the required files to with Spawner.
    - Divide logic for spawning enemy + Spawner handler logic

---
+++ Assets/Scripts/GameLogic/Spawner/Spawner.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using Utils.ArrayUtils;
+using static GameLogic.Spawner.EnemySpawner.EnemySpawnerHandler;
+
+namespace GameLogic.Spawner
+{
+    public class Spawner : MonoBehaviour
+    {
+        private ArrayTools.PseudoRandArray<SpawnAction> _waves;
+
+        private float _delayToNextSpawn = 5.0f;
+
+        private float _currentTimer;
+
+        private void Awake()
+        {
+            GameloopManager.instance.OnRestartGame += () =>
+            {
+                _currentTimer = _delayToNextSpawn;
+
+                SpawnAction[] waves = new SpawnAction[]{
+            new DasherSwarmSpawn(),
+            new BombrSpawn(),
+            new DasherSwarmSpawn(),
+            new EliteSpawn(),
+            new BombrSpawn(),
+            new BombrSpawn(),
+            new EliteSpawn(),
+            new DasherSwarmSpawn(),
+            new BombrSpawn(),
+            new DasherSwarmSpawn(),
+            new DasherSwarmSpawn(),
+            new BombrSpawn(),
+            new BombrSpawn(),
+            new DasherSwarmSpawn(),
+            new EliteSpawn(),
+            new BombrSpawn(),
+            new EliteSpawn(),
+        };
+
+
+                //NOTE FOR FUTURE LOGIC:
+                //I want to instead of create an array of attack waves, it should create automatically
+                //base on the variable(TimePassed since playing || Score || etc...)
+                //When all attack waves are done, create more of the them but with more difficulty
+
+                //IDEA: If x seconds passed in game, check how much enemies the player is killing,
+                //  if player is killing too much, spawn more enemies(Decrease start next wave)
+
+                _waves = new ArrayTools.PseudoRandArray<SpawnAction>(waves);
+            };
+
+            _currentTimer = _delayToNextSpawn;
+        }
+
+        private void Update()
+        {
+            if (_waves == null)
+            {
+                return;
+            }
+
+            _currentTimer -= Time.deltaTime;
+
+            if (_currentTimer <= 0.0f)
+            {
+                _currentTimer = _delayToNextSpawn;
+                _delayToNextSpawn -= 0.25f;
+
+                if (_delayToNextSpawn < 2.5f)
+                {
+                    _delayToNextSpawn = 2.5f;
+                }
+
+                _waves.PickNext().InvokNextSpawnAction();
+            }
+        }
+
+
+        public void ResetSpawnerProps()
+        {
+            _currentTimer = _delayToNextSpawn;
+        }
+
+        public void StartSpawner(Room roomToSpawn)
+        {
+            enabled = true;
+
+            SpawnAction[] waves = new SpawnAction[]{
+            new DasherSwarmSpawn(),
+            new DasherSwarmSpawn(),
+            new DasherSwarmSpawn(),
+            new BombrSpawn(),
+            new DasherSwarmSpawn(),
+            new EliteSpawn(),
+            new BombrSpawn(),
+            new BombrSpawn(),
+            new EliteSpawn(),
+            new DasherSwarmSpawn(),
+            new BombrSpawn(),
+            new DasherSwarmSpawn(),
+            new DasherSwarmSpawn(),
+            new BombrSpawn(),
+            new BombrSpawn(),
+            new DasherSwarmSpawn(),
+            new EliteSpawn(),
+            new BombrSpawn(),
+            new EliteSpawn(),
+    };
+
+
+            foreach (SpawnAction item in waves)
+            {
+                item.SetRoomToSpawn(roomToSpawn);
+            }
+
+
+            _waves = new ArrayTools.PseudoRandArray<SpawnAction>(waves);
+
+            _waves.PickNext().InvokNextSpawnAction();
+        }
+
+        public void StopSpawner()
+        {
+            _waves = null;
+            enabled = false;
+        }
+    }
+}
--- Assets/Scripts/Systems/GameloopManager.cs
+++ Assets/Scripts/Systems/GameloopManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Utils.GenericSingletons;
 using System;
+using GameLogic.Spawner;
 
 
 
