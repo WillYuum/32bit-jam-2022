07073db0f4c7db531e8bf3f8254a7b70ead441f2
WillYum <liamshere98@gmail.com>
2023-10-31 21:51:48 +0200

    Implement input logic for mobile

--- Assets/Scripts/GameLogic/PlayerBehavior/FishActions.cs
+++ Assets/Scripts/GameLogic/PlayerBehavior/FishActions.cs
@@ -8,34 +8,80 @@ public class FishMoveLogic
     public TurretPlatfromTracker TurretPlatfromTracker { get; private set; }
 
     private ITurretActions _turretActions;
+
+
+    private Action UpdateAction;
+
     public FishMoveLogic(Turret turret, ITurretActions turretActions)
     {
         _currentRotationDirection = RotationDirection.ClockWise;
         TurretPlatfromTracker = new TurretPlatfromTracker(turret);
 
         _turretActions = turretActions;
+
+        bool isMobile = Application.isMobilePlatform;
+
+        if (isMobile)
+        {
+            Vector2 leftSideScreen = new Vector2(Screen.width / 2, 0);
+            UpdateAction = () =>
+            {
+                //if click left side switch to anti clockwise
+                //if click right side switch to clockwise
+                if (Input.GetTouch(0).position.x > leftSideScreen.x)
+                {
+                    SetMoveDirection(TurretMoveDirection.AntiClockWise);
+                }
+                else
+                {
+                    SetMoveDirection(TurretMoveDirection.ClockWise);
+                }
+
+                if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    _turretActions.SetTurretMoveDirection(TurretMoveDirection.None);
+                }
+            };
+        }
+        else
+        {
+            UpdateAction = () =>
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    SetMoveDirection(TurretMoveDirection.ClockWise);
+
+
+                }
+                else if (Input.GetKeyDown(KeyCode.D))
+                {
+                    SetMoveDirection(TurretMoveDirection.AntiClockWise);
+
+                }
+
+                if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+                {
+                    _turretActions.SetTurretMoveDirection(TurretMoveDirection.None);
+                }
+
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+                {
+                    Move();
+                }
+            };
+        }
     }
 
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            _currentRotationDirection = RotationDirection.ClockWise;
-            _turretActions.SetTurretMoveDirection(TurretMoveDirection.ClockWise);
+        UpdateAction();
+    }
 
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _currentRotationDirection = RotationDirection.AntiClockWise;
-            _turretActions.SetTurretMoveDirection(TurretMoveDirection.AntiClockWise);
-        }
-
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            Move();
-        }
+    private void SetMoveDirection(TurretMoveDirection turretMoveDirection)
+    {
+        _currentRotationDirection = turretMoveDirection == TurretMoveDirection.ClockWise ? RotationDirection.ClockWise : RotationDirection.AntiClockWise;
+        _turretActions.SetTurretMoveDirection(turretMoveDirection);
     }
 
 
@@ -51,43 +97,65 @@ public class FishShootLogic
     private bool _shootToggle = false;
     public ShootController ShootController { get; private set; }
     private ITurretActions _turretActions;
+
+    private Action UpdateAction;
+
     public FishShootLogic(ITurretActions turretActions)
     {
         ShootController = new ShootController(GameVariables.instance.PlayerShootInterval);
 
         _turretActions = turretActions;
         if (_turretActions == null) Debug.LogError("Turret actions is null");
+
+        bool isMobile = Application.isMobilePlatform;
+
+        if (isMobile)
+        {
+            _shootToggle = true;
+            UpdateAction = () =>
+            {
+                if (ShootController.CanShootProjectile)
+                {
+                    Shoot();
+                }
+            };
+        }
+        else
+        {
+            UpdateAction = () =>
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _shootToggle = !_shootToggle;
+                    Debug.Log("Switching shoot toggle to: " + _shootToggle);
+                }
+
+                if (_shootToggle)
+                {
+                    Shoot();
+                }
+
+                if (Input.GetKeyDown(KeyCode.R) && ShootController.CanShootProjectile)
+                {
+                    UseExplosionAbility();
+                }
+            };
+        }
     }
 
     public void Update()
     {
         ShootController.UpdateTimer();
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _shootToggle = !_shootToggle;
-            Debug.Log("Switching shoot toggle to: " + _shootToggle);
-        }
 
-        if (_shootToggle)
-        {
-            Shoot();
-        }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            UseExplosionAbility();
-        }
+        UpdateAction();
     }
 
 
     private void Shoot()
     {
-        if (ShootController.CanShootProjectile)
-        {
-            _turretActions.PlayAnim();
-            ShootController.SetWaitTillShootAnimation(true);
-        }
+        _turretActions.PlayAnim();
+        ShootController.SetWaitTillShootAnimation(true);
     }
 
 
